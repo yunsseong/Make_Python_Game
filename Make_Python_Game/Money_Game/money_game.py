9a46801b20bad884d3654d9f17adb06fd747d4e8
정윤성 <ysjeong15@naver.com>
2021-07-10 04:02:41 +0000

    Add some code to prevent input error situations that user input wrong type of commend

--- Make_Python_Game/Money_Game/money_game.py
+++ Make_Python_Game/Money_Game/money_game.py
@@ -2,9 +2,10 @@ import random
 import sys
 
 global user_coin
-user_coin = 100
 
 def main_menu():
+    global user_coin
+    user_coin = 100
     print("$$ Welcom to Money Game $$")
     user_input = input("Wanna play some game? (Y/N) : ").lower()
     if user_input in ["y", "yes"]:
@@ -13,24 +14,34 @@ def main_menu():
         print("Your choice is good")
         sys.exit()
     else:
-        print("Hmm... You choice wrong option, Try again")
+        print("We only have two optins Yes Or No, Do you understand?")
         main_menu()
 
 def main_game_process():
     global user_coin
     print("$$---------------------------------------$$")
     print("You have ", user_coin, "coin(s), Insert the coins you want :")
-    bet_coin=int(input())
-    your_luck=random.randrange(1,101)
-    if your_luck > 50:
-        print("Oh you are lucky, Take your money")
-        user_coin += bet_coin *2
+    bet_coin=input()
+    if bet_coin.isdigit():
+        bet_coin=int(bet_coin)
+        your_luck=random.randrange(1,101)
+        if user_coin >= bet_coin:
+            if your_luck > 50:
+                print("Oh you are lucky, Take your money")
+                user_coin += bet_coin *2
+            else:
+                print("Oops This time was unlucky")
+                user_coin -= bet_coin *2
+            user_coin_check()
+        else:
+            print("HAHA Gotcha, How dare you cheet!")
+            main_game_process()
     else:
-        print("Oops This time was unlucky")
-        user_coin -= bet_coin *2
-    user_coin_check()
+        print("Hmm We only get number, Try again")
+        main_game_process()
 
 def user_coin_check():
+    global user_coin
     if user_coin <= 0:
         print("GAME OVER")
         main_menu()
